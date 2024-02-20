fc7f0d93cf85ed09cbc61ae395bfe07a8ca599ac
Jorge Dias <jloudias@gmail.com>
2024-02-19 22:32:46 -0300

    feat: radiobuttons on tutorial

--- customtkinter_tutorial/begginer2b.py
+++ customtkinter_tutorial/begginer2b.py
@@ -1,6 +1,38 @@
+from typing import Any, Tuple
 import customtkinter as ctk
 
-"""To reflect the layout in the code and follow the principle of always using classes from the intro, we will move the frame and checkbox code into a separate class. """
+"""Same as beginner2a.py, but implementing radio butons on frame_right """
+
+
+class MyRadioButtonFrame(ctk.CTkFrame):
+    def __init__(self, master, title, values):
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.values = values
+        self.title = title
+        self.radiobuttons = []
+        self.variable = ctk.StringVar(value="")
+
+        self.title = ctk.CTkLabel(
+            self,
+            text=self.title,
+            fg_color="gray30",
+            corner_radius=6,
+            text_color="white",
+        )
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+        for i, value in enumerate(self.values):
+            radiobutton = ctk.CTkRadioButton(
+                self, text=value, value=value, variable=self.variable
+            )
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.radiobuttons.append(radiobutton)
+
+    def get(self):
+        return self.variable.get()
+
+    def set(self, value):
+        self.variable.set(value)
 
 
 class MyCheckboxFrame(ctk.CTkFrame):
@@ -48,8 +80,8 @@ class App(ctk.CTk):
         self._set_appearance_mode("system")
         self.title("My App - OOP")
         self.geometry("400x250")
-        self.grid_columnconfigure(0, weight=1)  # expand column
-        self.grid_rowconfigure((0, 1), weight=1)  # expand rows
+        self.grid_columnconfigure((0, 1), weight=1)  # expand column
+        self.grid_rowconfigure(0, weight=1)  # expand rows
 
         # Checkbox Section -> moved to MyCheckboxFrame class
         self.checkbox_frame_left = MyCheckboxFrame(
@@ -58,13 +90,13 @@ class App(ctk.CTk):
             title="Programming Languages",
         )
         self.checkbox_frame_left.grid(
-            row=0, column=0, padx=10, pady=(10, 0), sticky="w"
+            row=0, column=0, padx=10, pady=(10, 0), sticky="nsew"
         )
-        self.checkbox_frame_right = MyCheckboxFrame(
-            self, values=["Doutorado", "Mestrado"], title="Graduação"
+        self.radio_frame_right = MyRadioButtonFrame(
+            self, values=["Junior", "Senior"], title="Programming Level"
         )
-        self.checkbox_frame_right.grid(
-            row=0, column=1, padx=10, pady=(10, 0), sticky="w"
+        self.radio_frame_right.grid(
+            row=0, column=1, padx=10, pady=(10, 0), sticky="nsew"
         )
 
         # Button Section
@@ -72,8 +104,8 @@ class App(ctk.CTk):
         self.button1.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
 
     def button_callback(self):
-        print("Checked left checkboxes: ", self.checkbox_frame_left.get())
-        print("Checked right checkboxes: ", self.checkbox_frame_right.get())
+        print("Checked: ", self.checkbox_frame_left.get())
+        print("Option: ", self.radio_frame_right.get())
 
 
 app = App()
