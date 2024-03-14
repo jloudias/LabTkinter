32359955ed3ba1e2dd78f683b9cd1149c33728ea
Jorge Dias <jloudias@gmail.com>
2024-03-13 21:30:31 -0300

    feat: add aula15_segmented_button.py

---
+++ customtkinter_tutorial/aula15_segmented_buttons.py
@@ -0,0 +1,95 @@
+import customtkinter as ctk
+from PIL import Image
+
+LABEL_FONT = ("Roboto", 20, "bold")
+APP_TITLE = "Aula 15 - Mastering Segmented Buttons"
+
+# images definitions
+image_sat = ctk.CTkImage(
+    # light_image=Image.open("images/satia1.jpg"),
+    dark_image=Image.open("images/satia1.jpg"),
+    size=(120, 120),
+)
+image_ju = ctk.CTkImage(
+    # light_image=Image.open("images/satia1.jpg"),
+    dark_image=Image.open("images/ju.jpg"),
+    size=(120, 120),
+)
+image_ss = ctk.CTkImage(
+    # light_image=Image.open("images/satia1.jpg"),
+    dark_image=Image.open("images/ss.jpg"),
+    size=(120, 120),
+)
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        ctk.set_appearance_mode("dark")
+
+        self.title("Segmented Buttons")
+        self.geometry("600x480")
+        self.top_title = TopTitle(self, APP_TITLE)
+        self.main_frame = Main(self)
+
+
+class TopTitle(ctk.CTkFrame):
+    def __init__(self, master, title):
+        super().__init__(master)
+
+        self.lbl_title = ctk.CTkLabel(
+            self, text=title, font=LABEL_FONT, text_color="orange"
+        ).pack(pady=40)
+
+        self.pack(fill="x")
+
+
+class Main(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        def show_image(value):
+
+            match value:
+                case "Sátia":
+                    v_image = image_sat
+                case "Sinthia e Sandro":
+                    v_image = image_ss
+                case "Jussara":
+                    v_image = image_ju
+
+            self.lbl_image.configure(image=v_image)
+            self.lbl_image.pack(pady=40)
+
+        self.btn_segmented = ctk.CTkSegmentedButton(
+            master=self,
+            width=140,
+            height=36,
+            corner_radius=None,
+            # border_width=3,
+            # bg_color="transparent",
+            # fg_color=None,
+            # selected_color=None,
+            # selected_hover_color=None,
+            # unselected_color=None,
+            # unselected_hover_color=None,
+            # text_color=None,
+            # text_color_disabled=None,
+            # background_corner_colors=None,
+            # font=None,
+            values=["Sátia", "Sinthia e Sandro", "Jussara"],
+            # variable=None,
+            # dynamic_resizing=True,
+            command=show_image,
+        )
+        # self.btn_segmented.set("Sátia")
+        self.btn_segmented.pack()
+
+        self.lbl_image = ctk.CTkLabel(self, text="", width=120, height=120)
+
+        self.pack(expand=True, fill="both")
+
+
+if __name__ == "__main__":
+    root = App()
+    root.mainloop()
--- customtkinter_tutorial/login_demo_oop.py
+++ customtkinter_tutorial/login_demo_oop.py
@@ -36,18 +36,31 @@ class Main(ctk.CTkFrame):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.v_username = ctk.StringVar()
+        self.v_password = ctk.StringVar()
 
+        self.lbl_username = ctk.CTkLabel(self, text="Enter your name").pack(
+            pady=(40, 0)
+        )
         self.username = ctk.CTkEntry(
-            master=self, placeholder_text="Username...", width=300, text_color="#FFCC70"
-        ).pack(pady=(40, 0))
+            master=self,
+            placeholder_text="Username...",
+            width=300,
+            text_color="#FFCC70",
+            textvariable=self.v_username,
+        ).pack(pady=(5, 0))
 
+        self.lbl_password = ctk.CTkLabel(self, text="Enter your password").pack(
+            pady=(20, 0)
+        )
         self.password = ctk.CTkEntry(
             master=self,
             placeholder_text="Password...",
             width=300,
             text_color="#FFCC70",
             show="*",
-        ).pack(pady=20)
+            textvariable=self.v_password,
+        ).pack(pady=5)
 
         ctk.CTkButton(
             master=self,
@@ -59,12 +72,14 @@ class Main(ctk.CTkFrame):
             border_color="#FFCC70",
             border_width=2,
             command=self.submit_form,
-        ).pack(pady=25)
+        ).pack(pady=(50, 0))
 
         self.place(x=0, y=80, relwidth=1, relheight=0.8)
 
     def submit_form(self):
-        CTkMessagebox(title="Info", message="  Working o it...  ")
+        # CTkMessagebox(title="Info", message="  Working o it...  ")
+        print(self.v_username.get())
+        print(self.v_password.get())
 
 
 if __name__ == "__main__":
